89ccee95c3b0292cb9b00503ed37a2d0b4c6bc14
daksh-rajora <dakshrajora11@gmail.com>
2026-07-06 13:25:29 +0530

    Just add some usable components and some changes

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
 import { Avatar, AvatarImage } from '../ui/avatar'
+import { LogOut, User2 } from 'lucide-react'
+import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+  const user = false
   return (
     <div className='bg-white'>
       <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
@@ -15,7 +19,15 @@ const Navbar = () => {
                 <li>Jobs</li>
                 <li>Browse</li>
             </ul>
-            <Popover>
+            {
+              !user ?(
+                <div className='flex items-center gap-2'>
+                  <Link to='/login'><Button variant='outline'>Login</Button>  </Link>
+                  <Link to='/signup'><Button className='bg-[#6A38C2] hover:bg-[#5b30a6]'>Signup</Button></Link>
+                </div>
+                
+              ):(
+                <Popover>
               <PopoverTrigger asChild>
                 <Avatar className='cursor-pointer' >
                   <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn"/>
@@ -31,11 +43,21 @@ const Navbar = () => {
                       <p className='text-sm text-muted-foreground'>Lorem ipsum dolor sit amet.</p>
                     </div>
                  </div>
-                 <div className='flex flex-col '>
-
+                 <div className='flex flex-col my-2 text-gray-600'>
+                    <div className='flex w-fit items-center gap-2 cursor-pointer'>
+                        <User2/>
+                        <Button variant='link'>View Profile</Button>
+                    </div>
+                    <div className='flex w-fit items-center gap-2 cursor-pointer'> 
+                      <LogOut/>
+                      <Button variant='link'>Logout</Button>
+                    </div>
                  </div>
               </PopoverContent>
             </Popover>
+              )
+            }
+            
               
             
         </div>
