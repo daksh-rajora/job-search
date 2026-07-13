02cb10180ebd3389b3fb5b38c9851aaf4654cbad
daksh-rajora <dakshrajora11@gmail.com>
2026-07-13 17:16:40 +0530

    completed the profile component

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import Navbar from "./shared/Navbar";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
-import { Pen } from "lucide-react";
+import { Contact, Mail, Pen } from "lucide-react";
+import { Badge } from "./ui/badge";
+import { Label } from "./ui/label";
+import AppliedJobTable from "./AppliedJobTable";
+
+const skills = ["java", "c++", "python", "react"];
 
 const Profile = () => {
+  const isResume = true;
+
   return (
     <div>
       <Navbar />
-      <div className="max-w-7xl mx-auto bg-white border border-gray-200 rounded-2xl my-5 p-8">
+      <div className="max-w-4xl mx-auto bg-white border border-gray-200 rounded-2xl my-5 p-8">
         <div className="flex justify-between">
           <div className="flex items-center  gap-4">
             <Avatar className="h-24 w-24">
@@ -21,12 +28,49 @@ const Profile = () => {
                 Ipsum,aut
               </p>
             </div>
-            
           </div>
-            <Button className="text-right" variant="outline">
-              <Pen />
-            </Button>
+          <Button className="text-right" variant="outline">
+            <Pen />
+          </Button>
         </div>
+        <div className="my-5">
+          <div className="flex items-center gap-3 my-2">
+            <Mail />
+            <span>max@gmail.com</span>
+          </div>
+          <div className="flex items-center gap-3 my-2">
+            <Contact />
+            <span>937489267</span>
+          </div>
+        </div>
+        <div className="my-5">
+          <h1>Skills</h1>
+          <div className="flex items-center gap-1">
+            {skills.length != 0 ? (
+              skills.map((item, index) => <Badge key={index}>{item}</Badge>)
+            ) : (
+              <span>No Skills Found</span>
+            )}
+          </div>
+        </div>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label className="text-md font-bold">Resume</Label>
+          {isResume ? (
+            <a
+              target="blank"
+              href="https://youtube.com"
+              className="text-blue-500 w-full hover:underline cursor-pointer"
+            >
+              Youtube
+            </a>
+          ) : (
+            <span>NA</span>
+          )}
+        </div>
+      </div>
+      <div className="max-w-4xl mx-auto bg-white rounded-2xl">
+        <h1 className="font-bold text-lg my-5">Applied Jobs</h1>
+        <AppliedJobTable />
       </div>
     </div>
   );
