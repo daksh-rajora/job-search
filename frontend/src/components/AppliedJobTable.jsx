930bc304637053a8dbf98bfe7cc9cc91ac84e3e5
daksh-rajora <dakshrajora11@gmail.com>
2026-07-13 17:15:56 +0530

    created new component

---
+++ frontend/src/components/AppliedJobTable.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './ui/table'
+import { Badge } from './ui/badge'
+
+const AppliedJobTable = () => {
+  return (
+    <div>
+      <Table>
+        <TableCaption>A list of your applied jobs</TableCaption>
+        <TableHeader>
+            <TableHead>Date</TableHead>
+            <TableHead>Job Role</TableHead>
+            <TableHead>Company</TableHead>
+            <TableHead className="text-right">Status</TableHead>
+        </TableHeader>
+        <TableBody>
+            {
+                [1,2,].map((item,index)=>(
+                    <TableRow key={index}>
+                        <TableCell>13-07-2025</TableCell>
+                        <TableCell>Frontend Developer</TableCell>
+                        <TableCell>TCS</TableCell>
+                        <TableCell className="text-right"><Badge>Selected</Badge></TableCell>
+                    </TableRow>
+                ))
+            }
+        </TableBody>
+      </Table>
+    </div>
+  )
+}
+
+export default AppliedJobTable
