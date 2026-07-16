1489a1f04fc5ba0cd2f93f091467f0370e6720ce
daksh-rajora <dakshrajora11@gmail.com>
2026-07-16 18:00:27 +0530

    create jobslice

---
+++ frontend/src/redux/jobSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const jobSlice = createSlice({
+    name: "job",
+    initialState: {
+        allJobs: [],
+        singleJob: null,
+        searchJobResult: [],
+        loading: false
+    },
+    reducers: {
+        //actions
+        setAllJobs: (state, action) => {
+            state.allJobs = action.payload;
+        },
+        setSingleJob: (state, action) => {
+            state.singleJob = action.payload;
+        },
+        setSearchJobResult: (state, action) => {
+            state.searchJobResult = action.payload;
+        },
+        setLoading: (state, action) => {
+            state.loading = action.payload;
+        }
+    }
+});
+
+export const {setAllJobs}= jobSlice.actions;
+export default jobSlice.reducer;
