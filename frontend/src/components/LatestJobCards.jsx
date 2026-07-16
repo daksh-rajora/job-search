097759c448fac7b3da27568f475b3ce7ecb6d172
daksh-rajora <dakshrajora11@gmail.com>
2026-07-16 17:59:49 +0530

    link with job

--- frontend/src/components/LatestJobCards.jsx
+++ frontend/src/components/LatestJobCards.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import { Badge } from './ui/badge'
+import { JOB_API_END_POINT } from '@/utils/constant'
 
-const LatestJobCards = () => {
+const LatestJobCards = ({job}) => {
   return (
     <div className='p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer'>
         <div>
-            <h1 className='font-medium text-lg'>Company Name</h1>
+            <h1 className='font-medium text-lg'>{job?.company?.name}</h1>
             <p className='text-sm text-gray-500'>India</p> 
         </div>
         <div>
-            <h1 className='font-bold text-lg my-2'>Job Title</h1>
-            <p className='text-sm text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <h1 className='font-bold text-lg my-2'>{job?.title}</h1>
+            <p className='text-sm text-gray-600'>{job?.description}</p>
         </div>
         <div className='flex items-center gap-2 mt-4'>
-            <Badge className="text-blue-700 font " variant='ghost'>12 Positions</Badge>
-            <Badge className="text-[#F83002] " variant='ghost'>Part Time</Badge>
-            <Badge className="text-[#7209b7] font " variant='ghost'>24 LPA </Badge>
+            <Badge className="text-blue-700 font " variant='ghost'>{job?.position}</Badge>
+            <Badge className="text-[#F83002] " variant='ghost'>{job?.jobType}</Badge>
+            <Badge className="text-[#7209b7] font " variant='ghost'>{job?.salary}</Badge>
         </div>
     </div>
   )
