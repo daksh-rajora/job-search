17c22b3137162695a4eaad7e115dd47644ccf900
daksh-rajora <dakshrajora11@gmail.com>
2026-07-16 18:00:09 +0530

    fixing profile

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -3,11 +3,33 @@ import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
 import { Avatar, AvatarImage } from '../ui/avatar'
 import { LogOut, User2 } from 'lucide-react'
 import { Button } from '../ui/button'
-import { Link, Links } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { Link, Links, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { toast } from 'sonner'
+import axios from 'axios'
+import { setUser } from '@/redux/authSlice'
+import { USER_API_END_POINT } from '@/utils/constant'
 
 const Navbar = () => {
   const {user}= useSelector(store => store.auth)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const logoutHandler = async () => {
+    try {
+      const res = await axios.get(`${USER_API_END_POINT}/logout`,{
+        withCredentials:true
+      })
+      if(res.data.success){
+        dispatch(setUser(null))
+        navigate('/')
+        toast.success(res.data.message)
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(error.response.data.message )
+    }
+  }
+
   return (
     <div className='bg-white'>
       <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
@@ -31,17 +53,17 @@ const Navbar = () => {
                 <Popover>
               <PopoverTrigger asChild>
                 <Avatar className='cursor-pointer' >
-                  <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn"/>
+                  <AvatarImage src={user?.profile?.profilePhoto} alt="@shadcn"/>
                 </Avatar>
               </PopoverTrigger>
               <PopoverContent className='w-80'>
                  <div className='flex items-center gap-4'>
                     <Avatar className='cursor-pointer' >
-                      <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn"/>
+                      <AvatarImage src={user?.profile?.profilePhoto} alt="@shadcn"/>
                     </Avatar>
                     <div>
-                      <h4 className='font-medium'>Daksh Rajora</h4>
-                      <p className='text-sm text-muted-foreground'>Lorem ipsum dolor sit amet.</p>
+                      <h4 className='font-medium'>{user?.fullname}</h4>
+                      <p className='text-sm text-muted-foreground'>{user?.profile?.bio}</p>
                     </div>
                  </div>
                  <div className='flex flex-col my-2 text-gray-600'>
@@ -51,7 +73,7 @@ const Navbar = () => {
                     </div>
                     <div className='flex w-fit items-center gap-2 cursor-pointer'> 
                       <LogOut/>
-                      <Button variant='link'>Logout</Button>
+                      <Button onClick={logoutHandler} variant='link'>Logout</Button>
                     </div>
                  </div>
               </PopoverContent>
