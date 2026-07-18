7ad2435e1c9f0ff7a88a6b58733c36cfb48a1462
daksh-rajora <dakshrajora11@gmail.com>
2026-07-18 18:34:17 +0530

    update

--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
+import { useParams } from "react-router-dom";
+import useGetSingleJob from "@/hooks/useGetSingleJob";
 
 const JobDescription = () => {
   const isApplied = true;
+  const params = useParams()
+  const jobId = params.id;
+  useGetSingleJob(jobId) //custom hook to get single job
   return (
     <div className="max-w-7xl mx-auto my-10">
       <div className="flex items-center justify-between">
