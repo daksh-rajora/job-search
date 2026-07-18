afe9b1206dae9a6d9d6c6a576ba3f05af2a27c7c
daksh-rajora <dakshrajora11@gmail.com>
2026-07-18 18:34:10 +0530

    update

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -5,7 +5,7 @@ import { Avatar, AvatarImage } from "./ui/avatar";
 import { Badge } from "./ui/badge";
 import { useNavigate } from "react-router-dom";
 
-const Job = () => {
+const Job = (job) => {
 
   const navigate = useNavigate()
   const jobId = "dfklsdjfsd";
@@ -26,25 +26,23 @@ const Job = () => {
           </Avatar>
         </Button>
         <div>
-          <h1 className="font-medium text-lg">Company Name</h1>
+          <h1 className="font-medium text-lg">{job?.company?.name}</h1>
           <h1 className="text-sm text-gray-500">India</h1>
         </div>
       </div>
       <div>
-        <h1 className="font-bold text-lg my-2">Title</h1>
+        <h1 className="font-bold text-lg my-2">{job?.title}</h1>
         <p className="text-sm text-gray-600">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi,
-          laborum. ipsum dolor sit amet consectetur adipisicing elit. Provident,
-          totam?
+          {job?.description}
         </p>
       </div>
       <div className="flex items-center gap-2 mt-4">
-        <Badge className="text-blue-700 font-bold" variant="ghost">12 Positions</Badge>
-        <Badge className="text-[#F83002] font-bold" variant="ghost">Part Time</Badge>
-        <Badge className="text-[#7209b7] font-bold" variant="ghost">24 LPA</Badge>
+        <Badge className="text-blue-700 font-bold" variant="ghost">{job?.position}</Badge>
+        <Badge className="text-[#F83002] font-bold" variant="ghost">{job?.jobType}</Badge>
+        <Badge className="text-[#7209b7] font-bold" variant="ghost">{job?.salary}</Badge>
       </div>
       <div className="flex items-center gap-4 mt-4">
-        <Button onClick={()=>navigate('/description/${jobId}')} variant="outline">Details</Button>
+        <Button onClick={()=>navigate('/description/${job?._id}')} variant="outline">Details</Button>
         <Button className="bg-[#7209b7]">Save For Later</Button>
       </div>
     </div>
