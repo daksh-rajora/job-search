4a6427de67970d113100541f48c6c6c98df8998a
daksh-rajora <dakshrajora11@gmail.com>
2026-07-21 18:46:39 +0530

    populate application

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -67,6 +67,8 @@ export const getJobById = async (req, res) => {
         const jobId = req.params.id;
         const job = await Job.findById(jobId).populate({
             path:"company"
+        }).populate({
+            path:"applications"
         });
         if(!job){
             return res.status(404).json({
