259dfee3365a97cb354d110f6bae5245c69da51c
daksh-rajora <dakshrajora11@gmail.com>
2026-07-23 17:50:35 +0530

    css changed

--- frontend/src/admin/Companies.jsx
+++ frontend/src/admin/Companies.jsx
@@ -12,7 +12,7 @@ const Companies = () => {
   return (
     <div>
         <Navbar/>
-        <div className='max-w-6xl mx-auto my-10'>
+        <div className='max-w-7xl mx-auto my-10 px-4'>
             <div className='flex items-center justify-between my-5'>
                 <Input
                     className='w-fit'
