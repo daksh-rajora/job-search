fb7cadc470f56424e0f2842bb7daa87c1f0621fc
daksh-rajora <dakshrajora11@gmail.com>
2026-07-25 18:56:53 +0530

    new code added

--- frontend/src/admin/Companies.jsx
+++ frontend/src/admin/Companies.jsx
@@ -1,15 +1,22 @@
 import Navbar from '@/components/shared/Navbar'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import CompaniesTable from './CompaniesTable'
 import { useNavigate } from 'react-router-dom'
 import useGetAllCompanies from '@/hooks/useGetAllCompanies'
+import { useDispatch } from 'react-redux'
+import { setSearchCompanyByText } from '@/redux/companySlice'
 
 const Companies = () => {
     useGetAllCompanies()
     const [input, setInput] = useState("")
     const navigate=useNavigate()
+    const dispatch= useDispatch()
+    
+    useEffect(()=>{
+        dispatch(setSearchCompanyByText(input))
+    },[input])
 
   return (
     <div>
