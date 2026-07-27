b8aa9e23a2d409d4e08691d5604d48ce3a5e2211
daksh-rajora <dakshrajora11@gmail.com>
2026-07-27 14:43:40 +0530

    css

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,26 +1,46 @@
 import { Search } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "./ui/button";
+import { useDispatch } from "react-redux";
+import { setSearchQuery } from "@/redux/jobSlice";
+import { useNavigate } from "react-router-dom";
 
 const HeroSection = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [query, setQuery] = useState("");
+
+  const searchJobHandler = () => {
+    dispatch(setSearchQuery(query));
+    navigate("/browse");
+  };
+
   return (
-    <div className="text-center">
+    <div className="text-center px-4">
       <div className="flex flex-col gap-5 my-10">
-        <span className="px-4 py-2 rounded-full mx-auto bg-gray-100 text-[#F83002]">
+        <span className="px-4 py-2 rounded-full mx-auto bg-gray-100 text-[#F83002] text-sm md:text-base">
           No. 1 Job Hunt Website
         </span>
-        <h1 className="text-5xl font-bold">
+        <h1 className="text-3xl md:text-5xl font-bold leading-tight">
           Search, Apply & <br />
           Get Your <span className="text-[#6A38C2]">Dream Jobs</span>
         </h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati fugit error illum corporis atque!</p>
-        <div className="flex w-[40%] shadow-lg border border-b-gray-200 pl-3 rounded-full items-center gap-4 mx-auto">
-          <input type="text"
-          placeholder="Find your dream jobs" 
-          className="outline-none border-none w-full"
+        <p className="text-sm md:text-base max-w-xl mx-auto text-gray-600">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
+          fugit error illum corporis atque!
+        </p>
+        <div className="flex w-full sm:w-[70%] md:w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto">
+          <input
+            type="text"
+            placeholder="Find your dream jobs"
+            className="outline-none border-none w-full py-2 text-sm md:text-base"
+            onChange={(e) => setQuery(e.target.value)}
           />
-          <Button className="rounded-r-full bg-[#6A38C2]">
-            <Search className="h-5 w-5"/>
+          <Button
+            onClick={searchJobHandler}
+            className="rounded-r-full bg-[#6A38C2] hover:bg-[#582fa1] py-2"
+          >
+            <Search className="h-5 w-5" />
           </Button>
         </div>
       </div>
