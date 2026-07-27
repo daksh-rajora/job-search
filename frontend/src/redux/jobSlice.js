d3c6ddc07848f90a7ba1f391ab9e90ae5c60a35f
daksh-rajora <dakshrajora11@gmail.com>
2026-07-27 14:50:39 +0530

    some changes

--- frontend/src/redux/jobSlice.js
+++ frontend/src/redux/jobSlice.js
@@ -1,37 +1,58 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const jobSlice = createSlice({
-    name: "job",
-    initialState: {
-        allJobs: [],
-        singleJob: null,
-        searchJobResult: [],
-        loading: false,
-        allAdminJobs:[],
-        searchJobByText:""
+  name: "job",
+  initialState: {
+    allJobs: [],
+    singleJob: null,
+    searchJobResult: [],
+    loading: false,
+    allAdminJobs: [],
+    searchJobByText: "",
+    allAppliedJobs: [],
+    searchQuery: "",
+    searchedQuery: "",
+  },
+  reducers: {
+    //actions
+    setAllJobs: (state, action) => {
+      state.allJobs = action.payload;
     },
-    reducers: {
-        //actions
-        setAllJobs: (state, action) => {
-            state.allJobs = action.payload;
-        },
-        setSingleJob: (state, action) => {
-            state.singleJob = action.payload;
-        },
-        setSearchJobResult: (state, action) => {
-            state.searchJobResult = action.payload;
-        },
-        setLoading: (state, action) => {
-            state.loading = action.payload;
-        },
-        setAllAdminJobs:(state,action)=>{
-            state.allAdminJobs= action.payload
-        },
-        setSearchJobByText:(state, action)=>{
-            state.searchJobByText=action.payload
-        }
-    }
+    setSingleJob: (state, action) => {
+      state.singleJob = action.payload;
+    },
+    setSearchJobResult: (state, action) => {
+      state.searchJobResult = action.payload;
+    },
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
+    setAllAdminJobs: (state, action) => {
+      state.allAdminJobs = action.payload;
+    },
+    setSearchJobByText: (state, action) => {
+      state.searchJobByText = action.payload;
+    },
+    setAllAppliedJobs: (state, action) => {
+      state.allAppliedJobs = action.payload;
+    },
+    setSearchQuery: (state, action) => {
+      state.searchQuery = action.payload;
+    },
+    setSearchedQuery: (state, action) => {
+      state.searchedQuery = action.payload;
+    },
+  },
 });
 
-export const {setAllJobs,setSearchJobByText,setAllAdminJobs,setSingleJob}= jobSlice.actions;
+export const {
+  setSearchQuery,
+  setSearchedQuery,
+  setAllAppliedJobs,
+  setAllJobs,
+  setSearchJobByText,
+  setAllAdminJobs,
+  setSingleJob,
+} = jobSlice.actions;
 export default jobSlice.reducer;
+
