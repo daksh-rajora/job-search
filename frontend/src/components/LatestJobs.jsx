3ace9eb93cf1c75fa36ef4a22314234a8a2e7551
daksh-rajora <dakshrajora11@gmail.com>
2026-07-27 14:49:24 +0530

    css

--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -7,10 +7,10 @@ import { useSelector } from 'react-redux'
 const LatestJobs = () => {
   const {allJobs} = useSelector(store=>store.job)
   return (
-    <div className='max-w-7xl mx-auto my-20 px-4'>
+    <div className='max-w-7xl mx-auto my-10 md:my-20 px-4'>
       <h1 className='text-4xl font-bold'><span className='text-[#6A38C2]'>Latest & Top</span> Job Openings</h1>
       {/* //multiple job cards display here */}
-      <div className='grid grid-cols-3 gap-4 my-5'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 my-5'>
         {
             allJobs.length <= 0 ? <span>No Job Available</span> : allJobs?.slice(0,6).map((job)=> <LatestJobCards key={job._id} job={job}/>)
         }
